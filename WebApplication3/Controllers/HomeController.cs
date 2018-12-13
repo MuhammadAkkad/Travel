03413e9730e41267ed15e18af11c4bf4ecdd3175
muhammadakkad <mhammad.akkad@gmail.com>
2018-12-13 18:47:06 +0300

    initial commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieDbContext context;
+        public HomeController(MovieDbContext c)
+        {
+            context = c;
+        }
+        public IActionResult Index()
+        {
+            //context.Movies.Add(new Movie { Name = "The Godfather", Desc = "asdsa" });
+            //context.SaveChanges(); 
+            //Movie m = context.Movies.FirstOrDefault(i=>i.Id==1);
+            //m.Name = "Esaretin Bedeli";
+            //context.Movies.Update(m);
+            //context.Save Changes();
+            return View(context.Movies.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            return View(context.Movies.FirstOrDefault(i=>i.MovieId==id));
+        }
+        public IActionResult Filter(int cid)
+        {
+            var movies = from m in context.Movies
+                         join mc in context.MovieCategories
+                         on m.MovieId equals mc.MovieId
+                         where mc.CategoryId == cid
+                         select m;
+
+            return View("Index",movies.ToList());
+        }
+    }
+}
---
+++ WebApplication3/Migrations/20181207155809_initial.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication3.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Desc = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false),
+                    MovieId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieCategories", x => new { x.CategoryId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_MovieCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieCategories_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieCategories_MovieId",
+                table: "MovieCategories",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieCategories");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
