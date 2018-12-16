b69d694ef8dd3eed22634a3c9b6ff347056ffa56
muhammadakkad <mhammad.akkad@gmail.com>
2018-12-16 13:20:42 +0300

    added new vars

--- WebApplication3/Models/Movie.cs
+++ WebApplication3/Models/Movie.cs
@@ -10,6 +10,10 @@ namespace WebApplication3.Models
         public int MovieId { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
+        public string TripSubtitle { get; set; }
+        public string Continent { get; set; }
+        public int price { get; set; }
+
 
 
         public List<MovieCategory> MovieCategories { get; set; }
